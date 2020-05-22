a4cfd79a94f71ae232b23e9f02e5ee557c54f8c8
C-70 <mumuhkustino@gmail.com>
2020-05-22 09:54:35 +0700

    Create Flexible Widget
    Menambahkan Flexible Widget

--- lib/main.dart
+++ lib/main.dart
@@ -66,19 +66,44 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(
               title: Text("Anonymous Method"),
             ),
-            body: Center(
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {});
-                },
-                child: AnimatedContainer(
-                  color: Color.fromARGB(255, random.nextInt(256),
-                      random.nextInt(256), random.nextInt(256)),
-                  duration: Duration(seconds: 1),
-                  width: 50.0 + random.nextInt(101),
-                  height: 50.0 + random.nextInt(101),
+            body: Column(
+              children: <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: Row(children: <Widget>[
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                          margin: EdgeInsets.all(5), color: 
+                          Colors.blueAccent),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                          margin: EdgeInsets.all(5), 
+                          color: Colors.cyanAccent),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                          margin: EdgeInsets.all(5),
+                          color: Colors.indigoAccent),
+                    ),
+                  ]),
                 ),
-              ),
+                Flexible(
+                    flex: 2,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.blueGrey,
+                    )),
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      margin: EdgeInsets.all(5),
+                      color: Colors.blueAccent,
+                    ))
+              ],
             )));
   }
 }
