47cc71485a7aba376ce4676ada1b5019d8907b30
C-70 <mumuhkustino@gmail.com>
2020-05-31 10:54:41 +0700

    Create Shared Preferences
    Menambahkan Shared Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,15 +12,23 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isON = false;
-  bool isClick = false;
-  double flexPadding = 5;
+  TextEditingController controller = TextEditingController(text: "No Name");
 
-  Widget myWidget = Container(
-    width: 200,
-    height: 100,
-    decoration: BoxDecoration(
-        color: Colors.red, border: Border.all(color: Colors.black, width: 3)),
-  );
+  void saveData() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    preferences.setString("name", controller.text);
+    preferences.setBool("isON", isON);
+  }
+
+  Future<String> getName() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString("name") ?? "";
+  }
+
+  Future<bool> getState() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getBool("isON") ?? false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,147 +41,34 @@ class _MyAppState extends State<MyApp> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            AnimatedSwitcher(
-              child: myWidget,
-              duration: Duration(seconds: 1),
-              transitionBuilder: (child, animation) => RotationTransition(
-                turns: animation,
-                child: child,
-              ),
+            TextField(
+              controller: controller,
             ),
             Switch(
-                activeColor: Colors.green,
-                inactiveThumbColor: Colors.red,
-                inactiveTrackColor: Colors.red[200],
-                value: isON,
-                onChanged: (newValue) {
+              value: isON,
+              onChanged: (newValue) {
+                setState(() {
                   isON = newValue;
-                  setState(() {
-                    if (isON)
-                      myWidget = Container(
-                        key: ValueKey(2),
-                        width: 200,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            border: Border.all(color: Colors.black, width: 3)),
-                      );
-                    /* myWidget = SizedBox(
-                        width: 200,
-                        height: 100,
-                        child: Center(
-                          child: Text(
-                            "Switch: ON",
-                            style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20),
-                          ),
-                        ),
-                      ); */
-                    else
-                      myWidget = Container(
-                        key: ValueKey(1),
-                        width: 200,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            border: Border.all(color: Colors.black, width: 3)),
-                      );
+                });
+              },
+            ),
+            RaisedButton(
+                child: Text("Save"),
+                onPressed: () {
+                  saveData();
+                }),
+            RaisedButton(
+                child: Text("Load"),
+                onPressed: () {
+                  getName().then((value) {
+                    controller.text = value;
+                    setState(() {});
+                  });
+                  getState().then((value) {
+                    isON = value;
+                    setState(() {});
                   });
                 }),
-            Flexible(
-                flex: 1,
-                child: Row(
-                  children: <Widget>[
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(flexPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              isClick = !isClick;
-                              setState(() {
-                                if (isClick)
-                                  flexPadding = 20;
-                                else
-                                  flexPadding = 5;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.red,
-                            ),
-                          ),
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(flexPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              isClick = !isClick;
-                              setState(() {
-                                if (isClick)
-                                  flexPadding = 30;
-                                else
-                                  flexPadding = 5;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.green,
-                            ),
-                          ),
-                        )),
-                  ],
-                )),
-            Flexible(
-                flex: 1,
-                child: Row(
-                  children: <Widget>[
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(flexPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              isClick = !isClick;
-                              setState(() {
-                                if (isClick)
-                                  flexPadding = 50;
-                                else
-                                  flexPadding = 5;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.blue,
-                            ),
-                          ),
-                        )),
-                    Flexible(
-                        flex: 1,
-                        child: AnimatedPadding(
-                          duration: Duration(seconds: 1),
-                          padding: EdgeInsets.all(flexPadding),
-                          child: GestureDetector(
-                            onTap: () {
-                              isClick = !isClick;
-                              setState(() {
-                                if (isClick)
-                                  flexPadding = 10;
-                                else
-                                  flexPadding = 5;
-                              });
-                            },
-                            child: Container(
-                              color: Colors.yellow,
-                            ),
-                          ),
-                        )),
-                  ],
-                ))
           ],
         ),
       ),
