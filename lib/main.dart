f026811ae3fdb6626a5dbeb1d05872918a962ae8
C-70 <mumuhkustino@gmail.com>
2020-05-31 10:05:51 +0700

    Update HTTP Request
    Mengubah main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Music and Font"),
+        title: Text("HTTP Request"),
       ),
       body: Center(
         child: Column(
